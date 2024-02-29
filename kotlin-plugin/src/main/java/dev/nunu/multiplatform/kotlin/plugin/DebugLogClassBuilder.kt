1bf1ae2a4ce38d4080711d62812976ffc412bb0b
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-03-01 06:50:39 +0900

    Add DebugLogClassBuilder

--- kotlin-plugin/src/main/java/dev/nunu/multiplatform/kotlin/plugin/DebugLogClassBuilder.kt
+++ kotlin-plugin/src/main/java/dev/nunu/multiplatform/kotlin/plugin/DebugLogClassBuilder.kt
@@ -7,7 +7,9 @@ import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.resolve.jvm.diagnostics.JvmDeclarationOrigin
 import org.jetbrains.org.objectweb.asm.MethodVisitor
 import org.jetbrains.org.objectweb.asm.Opcodes
+import org.jetbrains.org.objectweb.asm.Type
 import org.jetbrains.org.objectweb.asm.commons.InstructionAdapter
+import java.lang.StringBuilder
 
 class DebugLogClassBuilder(
     private val annotations: List<String>,
@@ -30,15 +32,97 @@ class DebugLogClassBuilder(
             override fun visitCode() {
                 super.visitCode()
                 InstructionAdapter(this).apply {
-                    TODO("on method entry")
+                    // method-trace-printing code
+                    invokestatic("j/l/System", "currentTimeMillis", "()J", false)
+                    // store-random-strat-time
+                    store(9001, Type.LONG_TYPE)
+                    getstatic("j/l/System", "out", "Ljava/io/PrintStream;")
+                    anew(Type.getType(StringBuilder::class.java))
+                    invokespecial(
+                        "j/l/StringBuilder",
+                        "<init>",
+                        "()V",
+                        false
+                    )
+                    visitLdcInsn("-> ${function.name}(")
+                    invokevirtual(
+                        "j/l/StringBuilder",
+                        "append",
+                        "(Lj/l/String;)Lj/l/SB;",
+                        false
+                    )
+                    function.valueParameters.forEachIndexed { index, valueParameterDescriptor ->
+                        visitLdcInsn("${valueParameterDescriptor.name}=")
+                        invokevirtual(
+                            "j/l/StringBuilder",
+                            "append",
+                            "(Lj/l/String;)Lj/l/SB;",
+                            false
+                        )
+                        visitVarInsn(Opcodes.ALOAD, index + 1)
+                        invokevirtual(
+                            "j/l/StringBuilder",
+                            "append",
+                            "(Lj/l/String;)Lj/l/SB;",
+                            false
+                        )
+                    }
+                    invokevirtual(
+                        "j/l/StringBuilder",
+                        "toString",
+                        "(Lj/l/String;)Lj/l/String;",
+                        false
+                    )
+                    invokevirtual(
+                        "j/l/PrintStream",
+                        "println",
+                        "(Lj/l/String;)V",
+                        false
+                    )
                 }
             }
 
             override fun visitInsn(opcode: Int) {
                 when (opcode) {
-                    Opcodes.RETURN -> {
+                    Opcodes.RETURN, Opcodes.ARETURN, Opcodes.IRETURN -> {
                         InstructionAdapter(this).apply {
-                            TODO("on method exit")
+                            anew(Type.getType(StringBuilder::class.java))
+                            dup()
+                            invokespecial(
+                                "j/l/StringBuilder",
+                                "<init>",
+                                "()V",
+                                false
+                            )
+                            visitLdcInsn("<- ${function.name} [ ran in")
+                            invokevirtual(
+                                "j/l/StringBuilder",
+                                "append",
+                                "(Lj/l/String;)Lj/l/StringBuilder;",
+                                false
+                            )
+                            invokestatic("j/l/System", "currentTimeMillis", "()J", false)
+                            load(9001, Type.LONG_TYPE)
+                            sub(Type.LONG_TYPE)
+                            invokevirtual(
+                                "j/l/StringBuilder",
+                                "append",
+                                "(J)Lj/l/StringBuilder;",
+                                false
+                            )
+                            visitLdcInsn("ms ]")
+                            invokevirtual(
+                                "j/l/StringBuilder",
+                                "append",
+                                "(Lj/l/String;)Lj/l/StringBuilder;",
+                                false
+                            )
+                            invokevirtual(
+                                "j/l/StringBuilder",
+                                "toString",
+                                "(Lj/l/String;)Lj/l/String;",
+                                false
+                            )
                         }
                     }
                 }
